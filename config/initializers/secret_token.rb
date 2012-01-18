52b63d115b88e36059fd70a04ad629940301fb2e
gbbourde <gbourdeau@hotmail.com>
2012-01-18 11:24:00 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NancyLincolnApp::Application.config.secret_token = '09cbc2d87012358d92ce0eef9ca4ec1a8e0a392374dad168242d5b6675fe1c71821c7525b55a44ff91f05591d71bc13b5744b43b29f6cb34c1ea9c32de63a325'
